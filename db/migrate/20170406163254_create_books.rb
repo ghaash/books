e0061139bd47e6618c3d0c87741247264252fe81
ghaash <daniel.silverman@me.com>
2017-04-06 09:38:08 -0700

    added db for books

--- db/migrate/20170406163254_create_books.rb
+++ db/migrate/20170406163254_create_books.rb
@@ -1,6 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :books do |t|
+      t.string :name
+      t.string :genre
+      t.string :page_length
 
       t.timestamps
     end
