2e877cde387ddcd5e63c3dc6b34a61a3fe549453
ghaash <daniel.silverman@me.com>
2017-04-06 11:07:04 -0700

    added routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   resources :ratings
   resources :authors
   resources :books
+
+  resources :books, only: [:show, :index, :new, :create, :edit] do
+  resources :authors, only: [:show, :index, :new] do
+    resources :ratings, only: [:show, :index, :new]
+  end
+end
+
+  root to: "book#index"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
