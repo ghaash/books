c99e9230b82ea4503e98065bd9888a9c114a235a
ghaash <daniel.silverman@me.com>
2017-04-10 23:51:25 -0700

    installed devise, set up url helpers for login logout in index

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'pry'
 gem 'dotenv'
-
+gem 'devise'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :ratings
   resources :books
 
   resources :books, only: [:show, :index, :new, :create, :edit] do
-  resources :authors, only: [:show, :index, :new, :create] do
-    resources :ratings, only: [:show, :index, :new]
+  resources :ratings, only: [:show, :index, :new]
   end
-end
+
 
   root to: "books#index"
 
