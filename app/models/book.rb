58483ddd730a3c99bfc5dd4ce5ebff35591a7927
ghaash <daniel.silverman@me.com>
2017-04-11 17:47:34 -0700

    added validations to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
-has_many :ratings
+  validates :title, presence: true, uniqueness: { scope: [:name], message: "Cannot have the same title" }
+  validates :author, presence: true, {scope: [:author], message: "Must have an author" }
+  validates :genre, presence: true, {scope: [:genre], message: "Must have an genre" }
+  validates :description, presence: true, {scope: [:description], message: "Must have an description" }
+  validates :page_length, presence: true, { scope: [:page_length], message: "Please put in an integer" }
+
+  has_many :ratings
 
   def ratings_attributes=(ratings_attributes)
     ratings_attributes.each do |i, ratings_attributes|
