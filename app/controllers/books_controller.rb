dc6bbdce85db7c50eec5878423dfdafb008d3984
ghaash <daniel.silverman@me.com>
2017-04-13 22:05:46 -0700

    removed js form book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,92 +2,67 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /books
-  # GET /books.json
   def index
-  # raise params.inspect
- #    @ratings = Rating.all
- #    if !params[:rating].blank?
- #   @books = Book.by_rating(params[:rating1], params[:rating2])
- # elsif !params[:date].blank?
- #    if params[:date] == "Today"
- #      # raise 'inside today params if block'.inspect
- #      @books = Book.today
- #    else
- #      @books = Book.yesterday
- #    end
-  if !params[:sort].blank?
-  elsif params[:sort] == "Title"
+    @ratings = Rating.all
+    if !params[:rating].blank?
+   @books = Book.by_rating(params[:rating1], params[:rating2])
+ elsif !params[:date].blank?
+    if params[:date] == "Today"
+      # raise 'inside today params if block'.inspect
+      @books = Book.today
+    else
+      @books = Book.yesterday
+    end
+  elsif !params[:sort].blank?
+    if params[:sort] == "Title"
       @books = Book.title_sort
     else
       @books = Book.author_sort
     end
  else
-   # if no filters are applied, show all posts
    @books = Book.all
  end
+end
 
-
-
-  # GET /books/1
-  # GET /books/1.json
   def show
   end
 
-  # GET /books/new
   def new
     @book = Book.new
     @book.ratings.build
   end
 
-  # GET /books/1/edit
   def edit
   end
 
-
-  # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
 
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
-
-  # def top_five
-  #   @top_books = Book.top_five
-  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
